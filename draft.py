99b0ab472efac8bfb32cb7d69f7591791ead8b01
冉申 <ranshen0519@icloud.com>
2021-11-27 20:51:14 +0800

    Update draft.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- draft.py
+++ draft.py
@@ -1,8 +1,9 @@
-a = input("type a number: ")
-b = input("type a number: ")
-a = int(a)
-b = int(b)
 try:
+    a = input("type a number: ")
+    b = input("type a number: ")
+    a = int(a)
+    b = int(b)
     print(a / b)
-except ZeroDivisionError:
+except (ZeroDivisionError, ValueError):
     print("b cannot by zero.")
+    print("invalid put.")
