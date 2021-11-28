635897e8de4346fcabbbb6066f5d02b29cb88e9b
冉申 <ranshen0519@icloud.com>
2021-11-28 10:44:15 +0800

    Update chapter_4.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- chapter_4.py
+++ chapter_4.py
@@ -1,16 +1,42 @@
+# 4-1
 def squared(x):
     return x ** 2
 
 print(squared(2))
 
+
+# 4-2
+print()
 def print_string(string):
     print(string)
 
 print_string('Testing: 1, 2, 3')
 
 
+# 4-3
+print()
 def add_mult(a, b, c, x=100, z=1000):
     return a + b + c * x * z
 
 result = add_mult(10, 15, 25)
 print(result)
+
+
+# 4-4
+print()
+def devide(x):
+    return x / 2
+
+
+def multiply(x):
+    return x * 4
+
+variable_1 = devide(4)
+variable_2 = multiply(variable_1)
+
+print(variable_1)
+print(variable_2)
+
+
+# 4-5
+print()
