6aeeb7d6524feff76225591008af55b8c5420434
冉申 <ranshen0519@icloud.com>
2021-11-28 10:36:02 +0800

    Delete draft.py
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

--- draft.py
+++
@@ -1,9 +0,0 @@
-try:
-    a = input("type a number: ")
-    b = input("type a number: ")
-    a = int(a)
-    b = int(b)
-    print(a / b)
-except (ZeroDivisionError, ValueError):
-    print("b cannot by zero.")
-    print("invalid put.")
